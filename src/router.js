ee7f7a9cb609eea6aa128cb90afda630af58fa23
xugang <371801080@qq.com>
2017-09-25 08:40:15 +0800

    fdg

--- src/router.js
+++ src/router.js
@@ -3,11 +3,12 @@ import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import products from './routes/products';
 
+
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Route path="/" component={IndexPage} />
-      <Route path="/products" component={products} />
+    <Router history={ history }>
+        <Route path="/" component={ IndexPage }/>
+        <Route path="/products" component={ products }/>
     </Router>
   );
 }

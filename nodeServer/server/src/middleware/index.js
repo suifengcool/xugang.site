923b1d6fc19a076a0ce66c56bfa04f4a67e4422e
xugang <371801080@qq.com>
2018-03-25 11:05:04 +0800

    d

--- nodeServer/server/src/middleware/index.js
+++ nodeServer/server/src/middleware/index.js
@@ -11,11 +11,12 @@ module.exports = (app) => {
   // app.use(errorHandler())
   // app.use(logger())
   app.use((ctx, next) => {
-    if (ctx.request.header.host.split(':')[0] === 'localhost' || ctx.request.header.host.split(':')[0] === '127.0.0.1') {
-      ctx.set('Access-Control-Allow-Origin', '*')
-    } else {
-      ctx.set('Access-Control-Allow-Origin', SystemConfig.HTTP_server_host)
-    }
+    ctx.set('Access-Control-Allow-Origin', '*')
+    // if (ctx.request.header.host.split(':')[0] === 'localhost' || ctx.request.header.host.split(':')[0] === '127.0.0.1') {
+    //   ctx.set('Access-Control-Allow-Origin', '*')
+    // } else {
+    //   ctx.set('Access-Control-Allow-Origin', SystemConfig.HTTP_server_host)
+    // }
     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
     ctx.set('Access-Control-Allow-Credentials', true) // 允许带上 cookie
